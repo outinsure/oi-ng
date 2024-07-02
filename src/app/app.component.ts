836e555235643ffffe53a2c2df50fa22a06bd526
Emeka Ashikodi <ashikodiemeka@gmail.com>
2024-07-02 18:58:41 -0400

    add sponsor section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,119 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
   title = 'outinsure';
   platinumSponsors = [
     {
-      photoUrl: '',
-      link: '',
+      photoUrl:
+        'https://i.cbc.ca/1.5084727.1554400614!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_940/ontario-government-trillium-logo.jpg',
+      websiteUrl: 'http://www.ontario.ca/',
     },
     {
-      photoUrl: '',
-      link: '',
-    }
+      photoUrl:
+        'https://unityproject.ca/wp-content/uploads/2022/06/city-of-london-logo-site-10-830x1024.png',
+      websiteUrl: 'https://london.ca/',
+    },
+    {
+      photoUrl:
+        'https://logowik.com/content/uploads/images/google-logo-2020.jpg',
+      websiteUrl: 'https://www.google.com',
+    },
+  ];
+  standardSponsors = [
+    {
+      photoUrl:
+        'https://i.cbc.ca/1.5084727.1554400614!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_940/ontario-government-trillium-logo.jpg',
+      websiteUrl: 'http://www.ontario.ca/',
+    },
+    {
+      photoUrl:
+        'https://logowik.com/content/uploads/images/google-logo-2020.jpg',
+      websiteUrl: 'https://www.google.com',
+    },
+    {
+      photoUrl:
+        'https://unityproject.ca/wp-content/uploads/2022/06/city-of-london-logo-site-10-830x1024.png',
+      websiteUrl: 'https://london.ca/',
+    },
+  ];
+  registeredPartners = [
+    {
+      photoUrl:
+        'https://logowik.com/content/uploads/images/google-logo-2020.jpg',
+      websiteUrl: 'https://www.google.com',
+    },
+    {
+      photoUrl:
+        'https://unityproject.ca/wp-content/uploads/2022/06/city-of-london-logo-site-10-830x1024.png',
+      websiteUrl: 'https://london.ca/',
+    },
+    {
+      photoUrl:
+        'https://i.cbc.ca/1.5084727.1554400614!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_940/ontario-government-trillium-logo.jpg',
+      websiteUrl: 'http://www.ontario.ca/',
+    },
+    {
+      photoUrl:
+        'https://logowik.com/content/uploads/images/google-logo-2020.jpg',
+      websiteUrl: 'https://www.google.com',
+    },
+    {
+      photoUrl:
+        'https://unityproject.ca/wp-content/uploads/2022/06/city-of-london-logo-site-10-830x1024.png',
+      websiteUrl: 'https://london.ca/',
+    },
+    {
+      photoUrl:
+        'https://i.cbc.ca/1.5084727.1554400614!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_940/ontario-government-trillium-logo.jpg',
+      websiteUrl: 'http://www.ontario.ca/',
+    },
+    {
+      photoUrl:
+        'https://logowik.com/content/uploads/images/google-logo-2020.jpg',
+      websiteUrl: 'https://www.google.com',
+    },
+    {
+      photoUrl:
+        'https://unityproject.ca/wp-content/uploads/2022/06/city-of-london-logo-site-10-830x1024.png',
+      websiteUrl: 'https://london.ca/',
+    },
+    {
+      photoUrl:
+        'https://i.cbc.ca/1.5084727.1554400614!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_940/ontario-government-trillium-logo.jpg',
+      websiteUrl: 'http://www.ontario.ca/',
+    },
+    {
+      photoUrl:
+        'https://logowik.com/content/uploads/images/google-logo-2020.jpg',
+      websiteUrl: 'https://www.google.com',
+    },
+    {
+      photoUrl:
+        'https://unityproject.ca/wp-content/uploads/2022/06/city-of-london-logo-site-10-830x1024.png',
+      websiteUrl: 'https://london.ca/',
+    },
+    {
+      photoUrl:
+        'https://i.cbc.ca/1.5084727.1554400614!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_940/ontario-government-trillium-logo.jpg',
+      websiteUrl: 'http://www.ontario.ca/',
+    },
+    {
+      photoUrl:
+        'https://logowik.com/content/uploads/images/google-logo-2020.jpg',
+      websiteUrl: 'https://www.google.com',
+    },
+    {
+      photoUrl:
+        'https://unityproject.ca/wp-content/uploads/2022/06/city-of-london-logo-site-10-830x1024.png',
+      websiteUrl: 'https://london.ca/',
+    },
+    {
+      photoUrl:
+        'https://i.cbc.ca/1.5084727.1554400614!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_940/ontario-government-trillium-logo.jpg',
+      websiteUrl: 'http://www.ontario.ca/',
+    },
   ];
 }
