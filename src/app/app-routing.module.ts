e3e46097967df122ddf45b43ce87def79a04b7eb
Onyeabor Ashikodi <ashikodiemeka@gmail.com>
2024-08-20 22:51:07 -0400

    add update account routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,20 +4,26 @@ import { AuthFormComponent } from './auth-form/auth-form.component';
 import { LandingComponent } from './landing/landing.component';
 import { AccountComponent } from './account/account.component';
 import { AccountInfoComponent } from './account/account-info.component';
+import { UpdateNameComponent } from './update-form/update-name.component';
 
 export const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'oi-auth', component: AuthFormComponent },
   { 
-    path: 'account', 
+    path: 'account',
     component: AccountComponent, 
     children: [
       {
         path: '',
         title: 'Basic Info',
         component: AccountInfoComponent
-      }
-    ] 
+      },
+      {
+        path: 'change-name',
+        title: 'Update Name',
+        component: UpdateNameComponent
+      },
+    ]
   },
 ];
 
---
+++ src/app/update-form/update-name.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'update-name',
+    templateUrl: './update-name.component.html'
+})
+export class UpdateNameComponent {
+
+}
